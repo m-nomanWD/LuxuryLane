f1f263f914cbad74f924992326dcae25fb6fb2f2
muhammad noman <mnomanwd@gmail.com>
2023-11-16 07:28:35 -0800

    package.json conflict
    package.json

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './store'
 import { Toaster } from 'react-hot-toast'
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <>
-    <Provider store={store}>
-      <Toaster />
-      <App />
-    </Provider>
-  </>
+ReactDOM.render(
+  <Provider store={store}>
+    <Toaster />
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
